196703da7c2ef9e67d7deec2a3f94e7fb5f54102
Andrew20Ayman <andrew13ayman@gmail.com>
2020-08-11 14:59:19 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './layout/side-bar/side-bar.component';
+import { HomeComponent } from './view/home/home.component';
+import { ProjectsComponent } from './view/projects/projects.component';
+import { AboutComponent } from './view/about/about.component';
+import { SkillsComponent } from './view/skills/skills.component';
 
+import { OwlModule } from "ngx-owl-carousel";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    HomeComponent,
+    ProjectsComponent,
+    AboutComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OwlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
